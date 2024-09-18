5c324e20d930b6b905703d41c8f3759eb494286e
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-09-18 09:52:13 -0300

    alt pastas

--- PythonGuanabara/ex002.py
+++
@@ -1,6 +0,0 @@
-nome = input('Qual o seu nome? ')
-print('É um prazer te conhecer, ', nome)
-
-# FORMA ALTERNATIVA DE FAZER
-nome = input('Qual o seu nome? ')
-print('É um prazer te conhecer, {}!'.format(nome))
