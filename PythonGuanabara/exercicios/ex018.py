f92a67e5e734ca274e8006ca6cee5a01d4b55c55
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-10-01 14:45:35 -0300

    sorteio de um nome - maneira correta de fazer

--- PythonGuanabara/exercicios/ex018.py
+++ PythonGuanabara/exercicios/ex018.py
@@ -1,20 +1,14 @@
 # sorteia um nome entre 4 alunos
-from random import randrange
+from random import choice
 
-aluno1 = input('Digite o nome do aluno 1: ')
-aluno2 = input('Digite o nome do aluno 2: ')
-aluno3 = input('Digite o nome do aluno 3: ')
-aluno4 = input('Digite o nome do aluno 4: ')
+a1 = input('Digite o nome do aluno 1: ')
+a2 = input('Digite o nome do aluno 2: ')
+a3 = input('Digite o nome do aluno 3: ')
+a4 = input('Digite o nome do aluno 4: ')
 
-sorteado = randrange(1, 4)
+alunos = [a1, a2, a3, a4]
+escolhido = choice(alunos)
+
+print('O aluno escolhido foi: {}'.format(escolhido))
 
 
-
-if sorteado == 1:
-    print('O aluno sorteado foi o {}, {}'.format(sorteado, aluno1))
-elif sorteado == 2:
-    print('O aluno sorteado foi o {}, {}'.format(sorteado, aluno2))
-elif sorteado == 3:
-    print('O aluno sorteado foi o {}, {}'.format(sorteado, aluno3))
-elif sorteado == 4:
-    print('O aluno sorteado foi o {}, {}'.format(sorteado, aluno4))
