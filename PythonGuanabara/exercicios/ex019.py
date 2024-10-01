6b63928c4e720ffab7e3e834c68bae7bd88d8efd
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-10-01 14:53:22 -0300

    embaralhamento de uma lista de strings

--- PythonGuanabara/exercicios/ex019.py
+++ PythonGuanabara/exercicios/ex019.py
@@ -1,14 +1,15 @@
 # sortear ordem de apresentação entre 4 alunos
 
-import random
+from random import shuffle
 
-aluno1 = input('Digite o nome do aluno 1')
-aluno2 = input('Digite o nome do aluno 2')
-aluno3 = input('Digite o nome do aluno 3')
-aluno4 = input('Digite o nome do aluno 4')
+a1 = input('Digite o nome do aluno 1: ')
+a2 = input('Digite o nome do aluno 2: ')
+a3 = input('Digite o nome do aluno 3: ')
+a4 = input('Digite o nome do aluno 4: ')
 
-alunos = [aluno1, aluno2, aluno3, aluno4]
+alunos = [a1, a2, a3, a4]
 
-ordemApre = random.shuffle(alunos)
+shuffle(alunos)
 
-print('A ordem de apresentação será: {}'.format(ordemApre))
+print('A ordem de apresentação será')
+print(alunos)
