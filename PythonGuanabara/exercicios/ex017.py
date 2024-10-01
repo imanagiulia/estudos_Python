ca3a768fb7c6ea88fa15de16dc8065708cd44e6f
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-10-01 14:34:03 -0300

    alteração formatação resultado / ex seno, cosseno, tangente

--- PythonGuanabara/exercicios/ex017.py
+++ PythonGuanabara/exercicios/ex017.py
@@ -4,6 +4,6 @@ from math import cos, sin, tan, radians
 
 ang = int(input('Digite o valor do Ângulo: '))
 
-print('O cosseno de {}graus é {}'.format(ang, cos(radians(ang))))
-print('O seno de {}graus é {}'.format(ang, sin(radians(ang))))
-print('A tangente de {}graus é {}'.format(ang, tan(radians(ang))))
+print('O cosseno de {}graus é {:.2f}'.format(ang, cos(radians(ang))))
+print('O seno de {}graus é {:.2f}'.format(ang, sin(radians(ang))))
+print('A tangente de {}graus é {:.2f}'.format(ang, tan(radians(ang))))
