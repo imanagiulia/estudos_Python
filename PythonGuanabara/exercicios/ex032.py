b3eaec8f0fee9d6fbd2f046da50b4e8adaf21a49
Ana Giulia Aguiar <166962330+imanagiulia@users.noreply.github.com>
2024-10-07 11:40:09 -0300

    verificar qual o maior e o menor entre 3 números - otimizado

--- PythonGuanabara/exercicios/ex032.py
+++ PythonGuanabara/exercicios/ex032.py
@@ -1,16 +1,17 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um outro número: '))
 n3 = int(input('Digite um outro número: '))
+menor = n1
+maior = n1
 
-if (n1 > n2 and n1 > n3) and (n2 > n3):
-    print('{} foi o maior número digitado e o menor foi {}'.format(n1, n3))
-elif (n1 > n2 and n1 > n3) and (n3 > n2):
-    print('{} foi o maior número digitado e o menor foi {}'.format(n1, n2))
-elif (n2 > n1 and n2 > n3) and (n1 > n3):
-    print('{} foi o maior número digitado e o menor foi {}'.format(n2, n3))
-elif (n2 > n1 and n2 > n3) and (n3 > n1):
-    print('{} foi o maior número digitado e o menor foi {}'.format(n2, n1))
-elif (n3 > n1 and n3 > n2) and (n1 > n2):
-    print('{} foi o maior número digitado e o menor foi {}'.format(n3, n2))
-else:
-    print('{} foi o maior número digitado e o menor foi {}'.format(n3, n1))
+if n2 > n3 and n2 > n1:
+    maior = n2
+elif n3 > n2 and n3 > n1:
+    maior = n3
+if n2 < n3 and n2 < n1:
+    menor = n2
+elif n3 < n2 and n3 < n1:
+    menor = n3
+
+print('O maior número digitado foi {}'.format(maior))
+print('O menor número digitado foi {}'.format(menor))
