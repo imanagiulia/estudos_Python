c565aa1c0a6a3691e90731f07d084b20cbda6474
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-10-23 17:48:58 -0300

    é primo?

---
+++ PythonGuanabara/exercicios/ex052.py
@@ -0,0 +1,20 @@
+# é primo?
+from math import sqrt
+n = int(input('Digite um número para saber se ele é primo: '))
+if n <= 1:
+    print('Não é primo!')
+elif n == 2:
+        print('É primo!')
+elif n > 2 and n % 2 == 0:
+    print('Não é primo!')
+else:
+    primo = True
+    for c in range(2, int(sqrt(n)), 2):
+        if n % c == 0:
+            primo = False
+            break
+
+    if primo:
+        print('É primo')
+    else:
+        print('Não é primo')
