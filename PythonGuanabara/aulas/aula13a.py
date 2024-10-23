12d46b8bf54150d37e9d463c3746bb64392feb83
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-10-23 16:22:16 -0300

    contagem regressiva

---
+++ PythonGuanabara/aulas/aula13a.py
@@ -0,0 +1,10 @@
+# estrutura de repetição for
+
+for c in range(0,3):
+    print('REPETIÇÃO {}'.format(c))
+
+for c in range(3, 0, -1):
+    print('REPETIÇÃO {}'.format(c))
+
+for c in range(0, 11, 2):
+    print('REPETIÇÃO {}'.format(c))
