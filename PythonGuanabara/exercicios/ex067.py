048dc718680c2c46260c2098395569c629397294
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-11-05 16:24:09 -0300

    tabuada v3.0

--- PythonGuanabara/exercicios/ex067.py
+++ PythonGuanabara/exercicios/ex067.py
@@ -1,6 +1,7 @@
 while True:
     n = int(input('Digite um número para saber a sua tabuada [número negativo encerra o programa]: '))
     if n < 0:
+        print('Programa encerrado. Volte sempre!')
         break
     for c in range(11):
         print(f'{n} x {c} = {n*c}')
