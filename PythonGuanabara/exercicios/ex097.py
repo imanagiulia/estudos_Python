565612675a2185ddf9afd6461002bd6fe7b8d837
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-11-28 10:57:46 -0300

    print especial

---
+++ PythonGuanabara/exercicios/ex097.py
@@ -0,0 +1,8 @@
+def escreva(msg):
+    print('-' *(len(msg) + 4))
+    print(f'  {msg}')
+    print('-' *(len(msg) + 4))
+
+escreva('Ana Giulia')
+escreva('naju')
+escreva('Estudado Pyhton com Guanabara')
