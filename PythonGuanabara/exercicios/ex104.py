4867ecc967e10d6d1af7683088dba8abcf39f509
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-12-03 10:58:39 -0300

    função parecida com print

---
+++ PythonGuanabara/exercicios/ex104.py
@@ -0,0 +1,12 @@
+def leiaInt(msg):
+    while True:
+        num = input(msg)
+        if num.isnumeric():
+            break
+        else:
+            print(f'\033[31m [ERRO] Digite um número inteiro válido.\033[0m')
+    return num
+
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}. ')
