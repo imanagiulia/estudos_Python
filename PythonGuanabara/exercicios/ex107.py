8796288b3c15dc45d3630086e5ee33251d29b377
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-12-04 11:06:31 -0300

    moedas com modulos

---
+++ PythonGuanabara/exercicios/ex107.py
@@ -0,0 +1,8 @@
+from modulos import moedas
+
+num = int(input('Digite um número: '))
+print(f'O dobro de R${num} é R${moedas.dobro(num)}')
+print(f'A metade de R${num} é R${moedas.metade(num)}')
+print(f'Aumentando 10%, temos R${moedas.aumentar(num, 10)}')
+print(f'Diminuindo 10%, temos R${moedas.diminuir(num, 10)}')
+
